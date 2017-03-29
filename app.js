a3d486fd50e5cf7afeb2ea93a025fce34a50c479
will <code@willworth.co.uk>
2017-03-29 12:22:23 +0200

    add bootstrap

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var app = express();
 
 var port = 5050;
 
+app.use(expres.static('public'));
 
 app.get('/', function(req, res){
    res.send("hello world! songuard!") ;
+});   
+
+app.get('/songs', function(req, res){
+   res.send("songs listed here!") ;
 });   
 
 app.listen(port, function(err){
