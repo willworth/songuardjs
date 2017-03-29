23cf4a0d46d633946d59e9a9aa88248bba226ee3
will <code@willworth.co.uk>
2017-03-29 11:18:33 +0200

    add basic routing

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require("express");
 
-var hi = "hello";
+var app = express();
 
-console.log(hi);
+var port = 5050;
+
+
+app.get('/', function(req, res){
+   res.send("hello world! songuard!") ;
+});   
+
+app.listen(port, function(err){
+    console.log('running server on port ' + port);
+});
+
